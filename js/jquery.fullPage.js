061507057719509b49a6480ea4945661903ec1aa
Prudhvi Rampey <gergteg777@gmail.com>
2016-07-04 16:27:00 +0530

    Activated horizontal slides

--- js/jquery.fullPage.js
+++ js/jquery.fullPage.js
@@ -766,7 +766,7 @@
         * Creates the control arrows for the given section
         */
         function createSlideArrows(section){
-            section.find(SLIDES_WRAPPER_SEL).after('<div class="' + SLIDES_ARROW_PREV + '"></div><div class="' + SLIDES_ARROW_NEXT + '"></div>');
+           /** section.find(SLIDES_WRAPPER_SEL).after('<div class="' + SLIDES_ARROW_PREV + '"></div><div class="' + SLIDES_ARROW_NEXT + '"></div>');
 
             if(options.controlArrowColor!='#fff'){
                 section.find(SLIDES_ARROW_NEXT_SEL).css('border-color', 'transparent transparent transparent '+options.controlArrowColor);
@@ -776,6 +776,7 @@
             if(!options.loopHorizontal){
                 section.find(SLIDES_ARROW_PREV_SEL).hide();
             }
+            **/
         }
 
         /**
